a3cb6dc3d4f7848e919c7dd3f23253bb40343c09
deisesalless <deisesalles18@gmail.com>
2025-04-10 23:02:50 -0300

    adição da barra de rolagem (scroll)

---
+++ aula09/main.py
@@ -0,0 +1,59 @@
+import flet as ft
+from custom_checkbox import Checkbox
+
+def main(page: ft.Page):
+
+    # Configurações da página
+    page.title = 'Minhas Tarefas'
+    page.theme_mode =  ft.ThemeMode.LIGHT
+    page.window.width = 450
+    page.window.height = 650
+    page.padding = ft.padding.only(top=20, left=20, right=20, bottom=20)
+
+    # se caso expandir a page o conteudo fica centralizado na page
+    page.horizontal_alignment = ft.CrossAxisAlignment.CENTER
+
+    #capturar a altura e largura da pagina do aplicativo
+    WIDTH: int = page.width
+    HEIGHT: int = page.height
+    print(WIDTH, HEIGHT)
+
+    def add_task(e):
+        if new_task.value == '':
+            # focus() retorna para o campo de digigar uma nova tarefa
+            new_task.focus()
+            return
+        
+        listas_task_coluna.controls.append(Checkbox(new_task.value))
+        new_task.value = ''
+        page.update()
+        new_task.focus()
+
+
+    # adicionado o botão ENTER do teclado para funcionar como salvar
+    new_task = ft.TextField(hint_text='Insira uma tarefa', expand=True, autofocus=True, on_submit=add_task)
+    
+    button_add_task = ft.FloatingActionButton(icon=ft.icons.ADD, on_click=add_task)
+
+    # adição do scroll: para manter o tamanho da tela e adicionar uma barra de rolagem
+    listas_task_coluna = ft.Column(height=HEIGHT-170, scroll=ft.ScrollMode.ADAPTIVE)
+
+
+    # isso aqui é columa de dentro dela tem cada linha
+    # a coluna coloca todas as linhas em uma unica linha
+    barra_coluna = ft.Column(
+        width=400,
+        controls=[
+            ft.Row(
+                controls=[
+                    new_task,
+                    button_add_task
+                ]
+            ),
+            listas_task_coluna
+        ]
+    )
+
+    page.add(barra_coluna)
+
+ft.app(target=main)
